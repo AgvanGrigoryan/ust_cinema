c1ba0b0e0aad4a953cc7a1bbb980d605f2045b18
AgvanGrigoryan <agvangrigoryan2003@gmail.com>
2023-03-25 14:31:03 +0400

    movie reviews get test

--- movies/urls.py
+++ movies/urls.py
@@ -4,6 +4,7 @@ from movies import views
 
 urlpatterns = [
     path('', views.MoviesListView.as_view(), name='movies'),
-    path(r'detail/<slug:slug>/', views.MovieDetailView.as_view(), name='movie_detail')
+    path('detail/<slug:slug>/', views.MovieDetailView.as_view(), name='movie_detail'),
+    path('review/<int:pk>/add', views.AddReview.as_view(), name="add_review")
 
 ]
--- movies/views.py
+++ movies/views.py
@@ -1,9 +1,9 @@
 from django.http import HttpResponseNotFound
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views.generic import ListView, DetailView
 from django.views.generic.base import View
 
-from movies.models import Movie
+from movies.models import Movie, Reviews
 
 
 class MoviesListView(ListView):
@@ -14,3 +14,13 @@ class MoviesListView(ListView):
 class MovieDetailView(DetailView):
     model = Movie
     slug_field = 'url'
+
+
+class AddReview(View):
+
+    def post(self, request, pk):
+        data = dict(request.POST)
+        print(data)
+        Reviews.objects.create(author_name=data['author_name'], email=data['email'], text=data['text'], movie=Movie.objects.get(pk=pk))
+        print(request.POST)
+        return redirect("movies")
