525e028d8eb8a98159592737c669a536ce80e6dc
AgvanGrigoryan <agvangrigoryan2003@gmail.com>
2023-03-26 16:35:50 +0400

    review add function edit(add reviws author)

--- movies/forms.py
+++ movies/forms.py
@@ -6,4 +6,4 @@ from movies.models import Reviews
 class ReviewForm(forms.ModelForm):
     class Meta:
         model = Reviews
-        fields = ('author', 'text')
+        fields = ('text',)
--- movies/views.py
+++ movies/views.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect
 from django.views.generic import ListView, DetailView
 from django.views.generic.base import View
@@ -15,14 +16,14 @@ class MovieDetailView(DetailView):
     model = Movie
     slug_field = 'url'
 
-
 class AddReview(View):
 
-    def post(self, request, pk):
+    def post(self, request, pk=None):
         form = ReviewForm(request.POST)
         movie = Movie.objects.get(pk=pk)
         if form.is_valid():
             form = form.save(commit=False)
             form.movie = movie
+            form.author = request.user
             form.save()
         return redirect(movie.get_absolute_url())
