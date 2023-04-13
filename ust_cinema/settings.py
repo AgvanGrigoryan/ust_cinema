51fd0d5055c8f41a1d19f8168cb29b655ffcedf9
AgvanGrigoryan <agvangrigoryan2003@gmail.com>
2023-04-14 02:24:54 +0400

    domain set

--- ust_cinema/settings.py
+++ ust_cinema/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['ust-cinema.vercel.app']
+ALLOWED_HOSTS = ['ust-cinema-ustaagvan.vercel.app']
 
 # Application definition
 
