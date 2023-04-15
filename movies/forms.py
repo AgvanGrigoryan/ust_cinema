597b6f51b5d374d26fc7dd7b39fec356b4d45cdc
AgvanGrigoryan <agvangrigoryan2003@gmail.com>
2023-04-15 14:13:44 +0400

    django-recaptcha install

--- movies/forms.py
+++ movies/forms.py
@@ -1,15 +1,20 @@
+from captcha.fields import ReCaptchaField
+from captcha.widgets import ReCaptchaV3
 from django import forms
-# from snowpenguin.django.recaptcha3.fields import ReCaptchaField
 
 from movies.models import Reviews, RatingStar, Rating
 
 
 class ReviewForm(forms.ModelForm):
-#     captcha = ReCaptchaField()
+    captcha = ReCaptchaField(widget=ReCaptchaV3(
+        attrs={
+            'required_score': 0.85
+        }
+    ))
 
     class Meta:
         model = Reviews
-        fields = ('text', 'parent')
+        fields = ('text', 'parent', 'captcha')
         widgets = {
             'text': forms.Textarea(attrs={"name": "text", "class": "form-control border", "id": "reviewText_input", "required": True}),
         }
--- ust_cinema/settings.py
+++ ust_cinema/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1', '.vercel.app']
 
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
 
     'ckeditor',
     'ckeditor_uploader',
+    'captcha',
     'movies',
     'users',
 ]
